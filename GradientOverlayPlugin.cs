77554a4439462b8ae5c34411eb410d849d3beb56
Sven-Erik Jonsson <svenerikj@gmail.com>
2015-05-16 10:22:09 +0200

    Optimizations and performance tuning.

--- GradientOverlayPlugin.cs
+++ GradientOverlayPlugin.cs
@@ -7,6 +7,7 @@ using ImageResizer.Configuration;
 using ImageResizer.ExtensionMethods;
 using ImageResizer.Plugins.GradientOverlay.Primitives;
 using ImageResizer.Plugins.GradientOverlay.Extensions;
+using System.Threading.Tasks;
 
 namespace ImageResizer.Plugins.GradientOverlay
 {
@@ -120,36 +121,48 @@ namespace ImageResizer.Plugins.GradientOverlay
             return settings;
         }
 
-        private const int ByteSize = 4;
         private const double InvertedByte = 1.0 / 255.0;
 
-        private unsafe void DrawLinearGradient(BitmapData bitmap, Settings settings)
+        private static unsafe void DrawLinearGradient(BitmapData bitmap, Settings settings)
         {
+            var bpp = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            var ibpp = 1.0 / bpp;
+
+            var h = bitmap.Height;
+            var w = bitmap.Width;
+            var wb = w * bpp;
+
+            var s = bitmap.Stride;
+            var s0 = (byte*)bitmap.Scan0;
+
             // Declare start and end positions as relative to image size
-            var from = new Vector(settings.Start.X * bitmap.Width, settings.Start.Y * bitmap.Height);
-            var to = new Vector(settings.End.X * bitmap.Width, settings.End.Y * bitmap.Height);
+            var fx = settings.Start.X * w;
+            var fy = settings.Start.Y * h;
+
+            var tx = settings.End.X * w;
+            var ty = settings.End.Y * h;
 
             // A vector representing a line from start to end
-            var ft = new Vector(to.X - from.X, to.Y - from.Y);
+            var ftx = tx - fx;
+            var fty = ty - fy;
 
             // This variable is an optimization
             // It precalculates the dot product of the line from start to end inversely so that
             // code can run using a multiplication instead of a division
             // as you might now multiplications are way faster
-            var invftdot = 1 / (ft.X * ft.X + ft.Y * ft.Y);
+            var iftd = 1.0 / (ftx * ftx + fty * fty);
 
             var c1 = settings.FirstColor;
             var c2 = settings.SecondColor;
 
             var clamp = settings.Clamp;
 
-            for (var y = 0; y < bitmap.Height; y++)
+            for (var y = 0; y < h; y++)
             {
-                var row = (byte*)bitmap.Scan0 + (y * bitmap.Stride);
-
-                for (var x = 0; x < bitmap.Width; x++)
+                var row = s0 + y * s;
+                for (var x = 0; x < w; x++)
                 {
-                    var p = x * ByteSize;
+                    var p = x * bpp;
                     var b = row[p];
                     var g = row[p + 1];
                     var r = row[p + 2];
@@ -158,13 +171,16 @@ namespace ImageResizer.Plugins.GradientOverlay
                     // Alpha is normally located at row[p + 3].
 
                     // Establish a vector from start to current position
-                    var fp = new Vector(x - from.X, y - from.Y);        
+                    // var fpx = x - fx;
+                    // var fpy = y - fy;
+                    // Inlined
 
                     // This rather unreadable part projects the vector fp
                     // to a line segment. k is 0 - 1 (percent) on that line.
                     // k could be negative or > 1, that means that
                     // the current point is outside of the bounds of ft
-                    var k = (fp.X * ft.X + fp.Y * ft.Y) * invftdot;
+                    var k = ((x - fx) * ftx + (y - fy) * fty) * iftd;
+                    var ik = 1.0 - k;
 
                     if (clamp > 0)
                         k += -clamp + (k * clamp);
@@ -190,7 +206,7 @@ namespace ImageResizer.Plugins.GradientOverlay
                         {
                             row[p] = c2.B;
                             row[p + 1] = c2.G;
-                            row[p + 2] = c2.R;                            
+                            row[p + 2] = c2.R;
                             continue;
                         }
 
@@ -198,57 +214,70 @@ namespace ImageResizer.Plugins.GradientOverlay
                     }
 
                     // Mix colors based on k-value, linearly
-                    var ta = c2.A * k + c1.A * (1.0 - k);
-                    var tr = c2.R * k + c1.R * (1.0 - k);
-                    var tg = c2.G * k + c1.G * (1.0 - k);
-                    var tb = c2.B * k + c1.B * (1.0 - k);
+                    var ta = c2.A * k + c1.A * ik;
+                    var tr = c2.R * k + c1.R * ik;
+                    var tg = c2.G * k + c1.G * ik;
+                    var tb = c2.B * k + c1.B * ik;
 
-                    var blend = ta * InvertedByte;
+                    var bl = ta * InvertedByte;
+                    var ibl = 1.0 - bl;
 
                     // Blend the colors back with original colors from image
-                    row[p]     = (byte)(b * (1.0 - blend) + tb * blend);    //Blue  0-255
-                    row[p + 1] = (byte)(g * (1.0 - blend) + tg * blend);    //Green 0-255
-                    row[p + 2] = (byte)(r * (1.0 - blend) + tr * blend);    //Red   0-255
+                    row[p] = (byte)(b * ibl + tb * bl);    //Blue  0-255
+                    row[p + 1] = (byte)(g * ibl + tg * bl);    //Green 0-255
+                    row[p + 2] = (byte)(r * ibl + tr * bl);    //Red   0-255
                 }
             }
         }
 
-        private unsafe void DrawRadialGradient(BitmapData bitmap, Settings settings)
+        private static unsafe void DrawRadialGradient(BitmapData bitmap, Settings settings)
         {
-            // Declare origin and end positions as relative to image size
-            var origin = new Vector(settings.Start.X * bitmap.Width, settings.Start.Y * bitmap.Height);
-            var end = new Vector(settings.End.X * bitmap.Width, settings.End.Y * bitmap.Height);
+            var bpp = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+
+            var h = bitmap.Height;
+            var w = bitmap.Width;
+
+            var s = bitmap.Stride;
+            var s0 = (byte*)bitmap.Scan0;
+
+            // Declare start and end positions as relative to image size
+            var fx = settings.Start.X * w;
+            var fy = settings.Start.Y * h;
+
+            var tx = settings.End.X * w;
+            var ty = settings.End.Y * h;
 
             // This variable is an optimization
             // It precalculates the distance of the endpoint inversely so that
             // code can run using a multiplication instead of a division
             // as you might now multiplications are way faster
-            var invradius = 1.0 / new Vector(origin.X - end.X, origin.Y - end.Y).Length();
+            var ir = 1.0 / Math.Sqrt((fx - tx) * (fx - tx) + (fy - ty) * (fy - ty));
 
             var c1 = settings.FirstColor;
             var c2 = settings.SecondColor;
 
             var clamp = settings.Clamp;
 
-            for (var y = 0; y < bitmap.Height; y++)
+            for (var y = 0; y < h; y++)
             {
-                var row = (byte*)bitmap.Scan0 + (y * bitmap.Stride);
-
-                for (var x = 0; x < bitmap.Width; x++)
+                var row = s0 + y * s;
+                for (var x = 0; x < w; x++)
                 {
-                    var p = x * ByteSize;
+                    var p = x * bpp;
                     var b = row[p];
                     var g = row[p + 1];
                     var r = row[p + 2];
 
                     // Establish a vector from origin to current position
-                    var fp = new Vector(x - origin.X, y - origin.Y);
+                    var fpx = x - fx;
+                    var fpy = y - fy;
 
                     // This gets a value k 0 - 1 based on the distance from origin
                     // Becomes a circular form by design
-                    var k = Math.Sqrt(fp.X*fp.X + fp.Y*fp.Y) * invradius;
+                    var k = Math.Sqrt(fpx * fpx + fpy * fpy) * ir;
+                    var ik = 1.0 - k;
 
-                    if (clamp > 0)
+                    if (clamp > 0.0)
                         k += -clamp + (k * clamp);
 
                     // If colors are opaque, no need for mixing.
@@ -269,7 +298,7 @@ namespace ImageResizer.Plugins.GradientOverlay
                     {
                         if (c2.A == 255)
                         {
-                            row[p] = c2.B; 
+                            row[p] = c2.B;
                             row[p + 1] = c2.G;
                             row[p + 2] = c2.R;
                             continue;
@@ -279,17 +308,18 @@ namespace ImageResizer.Plugins.GradientOverlay
                     }
 
                     // Mix colors based on k-value, linearly
-                    var ta = c2.A * k + c1.A * (1.0 - k);
-                    var tr = c2.R * k + c1.R * (1.0 - k);
-                    var tg = c2.G * k + c1.G * (1.0 - k);
-                    var tb = c2.B * k + c1.B * (1.0 - k);
+                    var ta = c2.A * k + c1.A * ik;
+                    var tr = c2.R * k + c1.R * ik;
+                    var tg = c2.G * k + c1.G * ik;
+                    var tb = c2.B * k + c1.B * ik;
 
-                    var blend = ta * InvertedByte;
+                    var bl = ta * InvertedByte;
+                    var ibl = 1.0 - bl;
 
                     // Blend the colors back with original colors from image
-                    row[p]     = (byte)(b * (1.0 - blend) + tb * blend);    //Blue  0-255
-                    row[p + 1] = (byte)(g * (1.0 - blend) + tg * blend);    //Green 0-255
-                    row[p + 2] = (byte)(r * (1.0 - blend) + tr * blend);    //Red   0-255
+                    row[p] = (byte)(b * ibl + tb * bl);    //Blue  0-255
+                    row[p + 1] = (byte)(g * ibl + tg * bl);    //Green 0-255
+                    row[p + 2] = (byte)(r * ibl + tr * bl);    //Red   0-255
                 }
             }
         }
